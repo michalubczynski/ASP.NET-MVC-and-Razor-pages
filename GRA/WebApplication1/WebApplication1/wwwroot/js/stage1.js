92a92c5d6061153f4cdaf1c38cc943f58771a38a
michalubczynski <michlub322@student.polsl.pl>
2022-11-04 15:42:29 +0100

    ok

---
+++ GRA/WebApplication1/WebApplication1/wwwroot/js/stage1.js
@@ -0,0 +1,18 @@
+﻿function startGame() {
+    //https://www.w3schools.com/graphics/tryit.asp?filename=trygame_default_gravity
+    myGameArea.start();
+}
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 5000;
+        this.canvas.height = 5000;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        //this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
