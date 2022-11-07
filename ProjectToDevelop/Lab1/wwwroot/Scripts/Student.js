8bdad688303fb38247f0358f7c3ddaed2a77772d
michalubczynski <michlub322@student.polsl.pl>
2022-11-08 00:10:00 +0100

    up

---
+++ ProjectToDevelop/Lab1/wwwroot/Scripts/Student.js
@@ -0,0 +1,22 @@
+﻿class Student {
+    constructor(imie, nazw, ocena) {
+        this.imie = imie;
+        this.nazw = nazw;
+        this.ocena = ocena;
+    }
+    static studenci;
+    toString() {
+        return `Imie: ${this.imie} Nazwisko: ${this.nazw} ocena: ${this.ocena} <br\>`;
+    }
+    static WyswietlStudenci() {
+        document.getElementById('tabela').innerHTML = this.studenci.toString();
+
+    }
+    static DodajStudent(im, naz, oc) {
+        this.studenci.push(new Student(im, naz, oc));
+    }
+}
+function create() {
+    Studenci_ = [new Student("Michal", "Iloch", 3), new Student("Michal", "Iloch", 4), new Student("Michal", "Iloch", 5), new Student("Michal", "Iloch", 2), new Student("Michal", "Iloch", 5)];
+    Student.studenci = Studenci_;
+}
---
+++ ProjectToDevelop/Lab1/wwwroot/Scripts/StudentPrototype.js
@@ -0,0 +1,14 @@
+﻿const Student = function(imie,nazwisko,ocena){
+    this.im = imie;
+    this.nazw = nazwisko;
+    this.oc = ocena;
+    Student.studenci;
+}
+
+Student.prototype.WyswietlStudenci = () => document.getElementById('tabela').innerHTML = Student.studenci.toString();
+Student.prototype.toString = function () { return `Imie: ${this.im} Nazwisko: ${this.nazw} ocena: ${this.oc} <br\>` };
+
+function create() {
+    let Student_ = [new Student("Michal", "Judyh", 4), new Student("Michal", "Judyh", 4), new Student("Michal", "Judyh", 4), new Student("Michal", "Judyh", 4), new Student("Michal", "Judyh", 4)];
+    Student.studenci = Student_;
+}
