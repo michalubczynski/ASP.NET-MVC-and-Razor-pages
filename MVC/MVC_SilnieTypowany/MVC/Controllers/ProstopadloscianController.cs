a5e69dc829091e918b0a58d3cc58fa40c03a1e43
michalubczynski <michlub322@student.polsl.pl>
2022-12-01 22:41:08 +0100

    MVC Hard

---
+++ MVC/MVC_SilnieTypowany/MVC/Controllers/ProstopadloscianController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProstopadloscianController : Controller
+    {
+        public IActionResult Index(Prostopadloscian prostopadloscian)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(prostopadloscian);
+            }
+            else return NoContent();
+        }
+    }
+}
---
+++ MVC/MVC_SilnieTypowany/MVC/Models/Prostopadloscian.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class Prostopadloscian
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive number allowed")]
+        public double Wysokosc { get; set; } = 1;
+        [RegularExpression(@"^[0-9]*[02468]$", ErrorMessage = "Podaj wartosc parzysta")]
+        public int Szerokosc { get; set; } 
+        public int Grubosc { get; set; } 
+        public double PolePowierzchni { get {return Szerokosc* Wysokosc; } }
+        public double Objetosc { get { return Szerokosc * Wysokosc; } }
+        public double DlugoscKrawedzi { get { return Szerokosc * Wysokosc*Grubosc; } }
+        public string[]? Obliczenia { get; set; }
+
+
+
+    }
+}
